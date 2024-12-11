67808bf6ad807c86fb27d557d745301bbb6c27f9
Erjean <erjean0310@outlook.com>
2024-12-11 22:03:24 +0800

    添加根据单词拼写精确查找单词功能

--- VocabVoyage_BackEnd/app/api/v1/endpoints/word.py
+++ VocabVoyage_BackEnd/app/api/v1/endpoints/word.py
@@ -1,12 +1,14 @@
+from http.client import HTTPException
+
 from fastapi import APIRouter, Query, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.session import get_db
-from app.crud.word import search_word_fuzzy, get_word_by_id
+from app.crud.word import search_word_fuzzy, get_word_by_id, get_word_by_spell
 
 router = APIRouter()
 
 
-@router.get("/search")
+@router.get("/search", summary="单词模糊搜索")
 async def search_word(
         query: str = Query(..., description="搜索内容"),
         db: AsyncSession = Depends(get_db)
@@ -19,3 +21,8 @@ async def get_word(word_id: int, db: AsyncSession = Depends(get_db)):
     return await get_word_by_id(word_id, db)
 
 
+@router.post("/", summary="根据单词拼写获取单词信息")
+async def get_word_detail(word_spell: str, db: AsyncSession = Depends(get_db)):
+    if not word_spell:
+        raise HTTPException(status_code=400, detail="单词拼写不能为空")
+    return await get_word_by_spell(word_spell, db)
--- VocabVoyage_BackEnd/app/crud/word.py
+++ VocabVoyage_BackEnd/app/crud/word.py
@@ -3,6 +3,7 @@ from sqlalchemy.future import select
 from sqlalchemy import func
 from app.models.word import Word
 from fastapi import HTTPException
+from fastapi.encoders import jsonable_encoder
 
 
 async def search_word_fuzzy(query: str, db: AsyncSession):
@@ -38,3 +39,19 @@ async def get_word_by_id(word_id: int, db: AsyncSession):
         print(str(e))
         raise HTTPException(status_code=500, detail=str(e))
 
+
+async def get_word_by_spell(spell: str, db: AsyncSession):
+    try:
+        result = await db.execute(
+            select(Word).filter(Word.spell == spell)
+        )
+        word = result.scalar()
+
+        if not word:
+            raise HTTPException(status_code=404, detail=f"Word '{spell}' not found")
+
+        jsonable_words = jsonable_encoder(word)
+        return jsonable_words
+    except Exception as e:
+        print(str(e))
+        raise HTTPException(status_code=500, detail=str(e))
