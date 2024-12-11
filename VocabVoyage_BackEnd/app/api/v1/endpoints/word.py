4ac89b352e6967a39aed27cbddf440ace1d1d1d4
Erjean <erjean0310@outlook.com>
2024-12-11 21:38:19 +0800

    添加根据 id 查找单词功能

--- VocabVoyage_BackEnd/app/api/v1/endpoints/word.py
+++ VocabVoyage_BackEnd/app/api/v1/endpoints/word.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Query, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.session import get_db
-from app.crud.word import search_word_fuzzy
+from app.crud.word import search_word_fuzzy, get_word_by_id
 
 router = APIRouter()
 
@@ -12,5 +12,10 @@ async def search_word(
         db: AsyncSession = Depends(get_db)
 ):
     return await search_word_fuzzy(query, db)
+
+
+@router.get("/{word_id}", summary="根据单词 id 获取单词信息")
+async def get_word(word_id: int, db: AsyncSession = Depends(get_db)):
+    return await get_word_by_id(word_id, db)
 
 
--- VocabVoyage_BackEnd/app/crud/word.py
+++ VocabVoyage_BackEnd/app/crud/word.py
@@ -3,7 +3,6 @@ from sqlalchemy.future import select
 from sqlalchemy import func
 from app.models.word import Word
 from fastapi import HTTPException
-from fastapi.encoders import jsonable_encoder
 
 
 async def search_word_fuzzy(query: str, db: AsyncSession):
@@ -20,3 +19,22 @@ async def search_word_fuzzy(query: str, db: AsyncSession):
     except Exception as e:
         print(str(e))
         raise HTTPException(status_code=500, detail=str(e))
+
+
+async def get_word_by_id(word_id: int, db: AsyncSession):
+    try:
+        # 执行查询，返回指定字段
+        result = await db.execute(
+            select(Word.spell, Word.meaning, Word.description).filter(Word.id == word_id)
+        )
+        word = result.first()  # 获取第一条记录
+
+        if not word:
+            raise HTTPException(status_code=404, detail=f"Word with id {word_id} not found")
+
+        res = {"spell": word.spell, "meaning": word.meaning, "description": word.description}
+        return res
+    except Exception as e:
+        print(str(e))
+        raise HTTPException(status_code=500, detail=str(e))
+
